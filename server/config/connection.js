03b83877df3d9c08d724124d327f592b166026de
Kyle Schwent <kshwiss@gmail.com>
2023-10-02 20:36:18 -0500

    uncommented a line in the connection.js page

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,7 @@ require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  // process.env.MONGODB_URI ||
-  "mongodb://127.0.0.1:27017/workout-stuff"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/workout-stuff"
 );
 
 module.exports = mongoose.connection;
